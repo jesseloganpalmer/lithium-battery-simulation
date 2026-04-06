9d66d4a63312ae7e5ac17864b6e3a48eab74ba37
jesseloganpalmer <jlp74@cam.ac.uk>
2026-04-06 22:25:07 +0100

    run

---
+++ src/lithium_battery_simulation/lithium.py
@@ -0,0 +1,21 @@
+import pybamm
+import matplotlib.pyplot as plt
+
+
+experiment = pybamm.Experiment(
+    [
+        (
+            "Discharge at C/10 for 10 hours or until 3.3 V",
+            "Rest for 1 hour",
+            "Charge at 1 A until 4.1 V",
+            "Hold at 4.1 V until 50 mA",
+            "Rest for 1 hour",
+        )
+    ]
+    * 3,
+)
+
+model = pybamm.lithium_ion.DFN()  # Doyle-Fuller-Newman model
+sim = pybamm.Simulation(model, experiment=experiment)
+sim.solve([0, 10 * 3600])  # solve for 10 hours
+sim.plot()
